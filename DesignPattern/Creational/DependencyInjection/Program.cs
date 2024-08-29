a7e488ecc827e984ce16ff2b89ae7a223e8e7893
David Ko <davidko20160606@gmail.com>
2024-08-29 17:02:48 +0800

    [Add] Add Dependency Injection pattern

---
+++ DesignPattern/Creational/DependencyInjection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Creational.DependencyInjection
+{
+
+    // DI is a design pattern that allows objects to receive their dependencies
+    // from an external source rather than creating them internally
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TradeService ts = new TradeService(new TradeRepository());
+            ts.Process();
+        }
+    }
+
+    public class TradeService
+    {
+        private readonly TradeRepository tradeRepository;
+
+        public TradeService(TradeRepository tradeRepository)
+        {
+            this.tradeRepository = tradeRepository;
+        }
+
+        public void Process()
+        {
+            Console.WriteLine("Trade processed");
+            tradeRepository.UpdateTradeStatus("Live");
+        }
+    }
+
+    public class TradeRepository
+    {
+        public void UpdateTradeStatus(string status)
+        {
+            Console.WriteLine("Status Updated");
+        }
+    }
+}
